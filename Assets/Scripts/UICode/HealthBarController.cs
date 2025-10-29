98c3445799870cc38e470d8882a36d7d7b3c6658
AvianSteel <bwelch1@mail.bradley.edu>
2025-10-29 12:41:55 -0500

    Health Bar Setup

---
+++ Assets/Scripts/UICode/HealthBarController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class HealthBarController : MonoBehaviour
+{
+    public GameObject healthBar;
+    private int healthBarWidth;
+    private int healthBarPercent;
+    public int health;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
