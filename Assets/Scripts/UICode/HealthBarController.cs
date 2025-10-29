b931a04088c9406652902a6d1533fd5cdba854f8
AvianSteel <bwelch1@mail.bradley.edu>
2025-10-29 16:30:59 -0500

    Health bar and ability UI base functions

--- Assets/Scripts/UICode/HealthBarController.cs
+++ Assets/Scripts/UICode/HealthBarController.cs
@@ -4,18 +4,25 @@ using UnityEngine.Rendering;
 public class HealthBarController : MonoBehaviour
 {
     public GameObject healthBar;
+    public RectTransform hBar;
     private int healthBarWidth;
     private int healthBarPercent;
     public int health;
+    public PlayerControler playerControler;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        health = playerControler.hp;
+        healthBarPercent = health * 4;
+        healthBarWidth = 525;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        health = playerControler.hp;
+        healthBarPercent = health * 4;
+        healthBarWidth = (int)(5.25 * healthBarPercent);
+        hBar.sizeDelta = new Vector2(healthBarWidth, 56);
     }
 }
