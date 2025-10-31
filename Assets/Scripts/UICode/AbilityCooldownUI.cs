a984d8434f022ac81e38bde52f142c2c735b9b19
AvianSteel <bwelch1@mail.bradley.edu>
2025-10-31 16:53:38 -0500

    Revert "Reapply "ScoreUI and Ability CooldownUI when starting""
    This reverts commit aaea01efdcd9e20cb92ed877bdfc9e3160359e45.

--- Assets/Scripts/UICode/AbilityCooldownUI.cs
+++ Assets/Scripts/UICode/AbilityCooldownUI.cs
@@ -10,17 +10,12 @@ public class AbilityCooldownUI : MonoBehaviour
     public bool canLaserLocal;
     public bool canMineLocal;
     public bool canDashLocal;
-
-    private int laserLvlLocal;
-    private int mineLvlLocal;
-    private int dashLvlLocal;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         canLaserLocal = playerControler.canLaser;
         canMineLocal = playerControler.canMine;
         canDashLocal = playerControler.canDash;
-
     }
 
     // Update is called once per frame
@@ -30,22 +25,8 @@ public class AbilityCooldownUI : MonoBehaviour
         canMineLocal = playerControler.canMine;
         canDashLocal = playerControler.canDash;
 
-        laserLvlLocal = playerControler.laserLvl;
-        mineLvlLocal = playerControler.mineLvl;
-        dashLvlLocal = playerControler.dashLvl;
-
-        if(laserLvlLocal > 0)
-        {
-            laserRect.SetActive(!canLaserLocal);
-        }
-        if(mineLvlLocal > 0)
-        {
-            mineRect.SetActive(!canMineLocal);
-        }
-        if(dashLvlLocal > 0)
-        {
-            dashRect.SetActive(!canDashLocal);
-        }
-
+        laserRect.SetActive(!canLaserLocal);
+        mineRect.SetActive(!canMineLocal);
+        dashRect.SetActive(!canDashLocal);
     }
 }
