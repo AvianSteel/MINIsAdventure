87532f734a28cd56b30867cf77a41abd1a264243
bstom1 <bstomberg6@gmail.com>
2025-10-30 20:16:29 -0500

    Pause Menu programmed

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,16 +1,81 @@
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private PlayerMove playerMove;
+    private InputAction pauseMenu;
+    private bool isPaused;
+    [SerializeField] private GameObject pauseUI;
+    [SerializeField] private GameObject controlsUI;
+    
+
+
+    private void Awake()
     {
-        
+        playerMove = new PlayerMove();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        pauseMenu = playerMove.Player.Pause;
+        pauseMenu.Enable();
+
+        pauseMenu.performed += Pause;
+    }
+
+    private void OnDisable()
+    {
+        pauseMenu.Disable();
+    }
+
+    void Pause(InputAction.CallbackContext context)
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+
+    void ActivateMenu()
+    {
+        Time.timeScale = 0f;
+        pauseUI.SetActive(true);
+    }
+
+    void DeactivateMenu()
+    {
+        Time.timeScale = 1f;
+        pauseUI.SetActive(false);
+        controlsUI.SetActive(false);
+        isPaused = false;
+    }
+
+    public void ResumeButton()
+    {
+        DeactivateMenu();
+    }
+
+    public void ControlsButton()
+    {
+        controlsUI.SetActive(true);
+    }
+
+    public void QuitButton()
+    {
+        SceneManager.LoadScene("Alpha");
+    }
+
+    public void BackButton()
+    {
+        controlsUI.SetActive(false);
     }
 }
