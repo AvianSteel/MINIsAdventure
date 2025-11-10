9070dcd77267897baebd8d2807818d0309101f94
AvianSteel <bwelch1@mail.bradley.edu>
2025-11-10 11:40:26 -0600

    HealthBarWorking

--- Assets/Scripts/UICode/HealthBarController.cs
+++ Assets/Scripts/UICode/HealthBarController.cs
@@ -1,28 +1,24 @@
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class HealthBarController : MonoBehaviour
 {
-    public GameObject healthBar;
-    public RectTransform hBar;
-    private int healthBarWidth;
-    private int healthBarPercent;
+    public UnityEngine.UI.Slider healthBarSlider;
     public int health;
     public PlayerControler playerControler;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         health = playerControler.hp;
-        healthBarPercent = health * 4;
-        healthBarWidth = 525;
+        healthBarSlider.value = health;
     }
 
     // Update is called once per frame
     void Update()
     {
         health = playerControler.hp;
-        healthBarPercent = health * 4;
-        healthBarWidth = (int)(5.25 * healthBarPercent);
-        hBar.sizeDelta = new Vector2(healthBarWidth, 56);
+        healthBarSlider.value = health;
     }
 }
